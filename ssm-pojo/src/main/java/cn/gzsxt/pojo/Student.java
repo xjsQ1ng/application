3eb4998f9ec0a728c070bac978715d797bce8e18
shan <464949577@qq.com>
2018-11-10 22:45:35 +0800

    首次提交ssm-pojo聚合项目

---
+++ ssm-pojo/src/main/java/cn/gzsxt/pojo/Student.java
@@ -0,0 +1,33 @@
+package cn.gzsxt.pojo;
+
+public class Student {
+
+	private Long studentId;
+	private String studentName;
+	private String studentPwd;
+
+	public Long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentPwd() {
+		return studentPwd;
+	}
+
+	public void setStudentPwd(String studentPwd) {
+		this.studentPwd = studentPwd;
+	}
+
+}
