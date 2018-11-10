09522a79ebb092b52b1aead34f208393867446e9
shan <464949577@qq.com>
2018-11-10 20:34:52 +0800

    首次提交

---
+++ exercise2ssh/src/main/java/cn/xjs/controller/PrintController.java
@@ -0,0 +1,19 @@
+package cn.xjs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PrintController {
+
+	
+	@RequestMapping("/print/{messager}")
+	public String hello(@PathVariable String messager){
+		
+		System.out.println(messager);
+		
+		return "/index.jsp";
+	}
+	
+}
